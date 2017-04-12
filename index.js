3258f817ba4302efb4e629fc74068564b05276c3
Song Chen <schen2315@bths.edu>
2017-04-12 15:52:39 -0400

    added CORS permission for all yt pages

--- index.js
+++ index.js
@@ -10,7 +10,12 @@
 // $(document).ready(function() {
 // 	sound.play();
 // });
+var baseurl = "https://ixwyi5tpad.execute-api.us-east-1.amazonaws.com/dev";
 var mediaTypes = ["audio/webm", "audio/ogg", "audio/x-ogg", "audio/mp4", "audio/m4a", "audio/mp3", "audio/mpeg", "audio/wav", "audio/x-wav", "audio/flac", "audio/x-flac"];
+var container = {
+	"webm": "audio/webm",
+	"mp4": "audio/mp4"
+}
 // var url = "https://r11---sn-ab5l6n7d.googlevideo.com/videoplayback?source=youtube&mn=sn-ab5l6n7d&dur=225.697&ip=149.125.64.168&initcwndbps=1477500&ipbits=0&pl=22&mv=m&ms=au&mm=31&key=yt6&id=o-AHnpAFumeV_G2kfNE17c3jHWa1XM7wmpGIGFdXGe9EVW&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&upn=EnxqBf86eeU&mime=video%2Fmp4&itag=22&requiressl=yes&lmt=1472373274637338&expire=1490689859&mt=1490668164&ratebypass=yes&signature=112A7B53E77D270D8857267C1A9B00D5082E2035.2CE6014BBD46E8D7E742643C08805CED6FCA7521";
 var url = "afraidofheights.mp4";
 var sound;
@@ -20,30 +25,28 @@ var snippet = undefined;
 /*typing timeout variable*/
 var typeTO;
 var typeTimer = 800;
-$(document).ready(function() {
-	
-});
-//$(document).ready(pageLoadComplete);
+// $(document).ready(function() {
+// 	// var query = "https://ixwyi5tpad.execute-api.us-east-1.amazonaws.com/dev?v=T60Du8Py9yA";
+// 	// $.get(query, function(data) {
+// 	// 	console.log(data)
+// 	// })
+// });
+$(document).ready(pageLoadComplete);
 function pageLoadComplete() {
-	$status = $("div.status");
-	$input = $("div.input");
-	$start = $($input.find("input")[0]);
-	$stop = $($input.find("input")[1]);
-	$rate_input =  $($input.find("input")[2]);
-	$rate_p = $($input.find("p")[2]);
-	$input.hide();
+	var $status = $("div.status"),
+	$input = $("div.input"),
+	$start = $($input.find("input")[0]),
+	$stop = $($input.find("input")[1]),
+	$rate_input =  $($input.find("input")[2]),
+	$rate_p = $($input.find("p")[2]),
+	url = "",
+	source = "",
+	dataUri = "";
+	
 
-	// var xhr = new XMLHttpRequest();
-	// xhr.open('GET', url, true);
-	// xhr.responseType = 'arraybuffer';
-	// xhr.onload = function(e) {
-	// 	if(this.status == 200) {
-	// 		loadAudio();
-	// 	}
-	// }
-	// xhr.send();
+	init();
 
-	loadAudio();
+	//loadAudio();
 	//listener methods:
 	$(document).keyup(function(e) {
 		//console.log(snippet);
@@ -66,7 +69,7 @@ function pageLoadComplete() {
 	/*offset and duration should be in milliseconds*/
 	function loadAudio(offset, duration, rate) {
 			//var dataUri = "data:" + mediaTypes[3] + ";base64," + base64ArrayBuffer(xhr.response);
-			var dataUri = url;
+			//var dataUri = url;
 			rate = rate !== undefined ? rate : 1.0;
 			if(offset == undefined || duration == undefined) sound = new Howl({src: [dataUri]});
 			else {
@@ -145,10 +148,62 @@ function pageLoadComplete() {
 	}
 	//write a function to test whether the user is still typing
 	//if after a set # of seconds, if the keyup event has not fired
-
+	//clear the "typing" notification
 	function hasTypeCompleted() {
 		if(typeTO != undefined) clearTimeout(typeTO);
-		$status.find("p").text("typing");
+		$status.find("p").text("typing...");
 		typeTO = setTimeout(setAudio, typeTimer);
 	}
+
+	function init() {
+		$input.hide();
+		chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+		    url = tabs[0].url;
+		    var url_parameters = url.split('?');
+		    url = baseurl + '?';
+		    for(var i=1; i < url_parameters.length; i++) {
+		    	url += url_parameters[i];
+		    }
+		    //console.log(url);
+		    $.get(url, function(data) {
+		    	console.log(data);
+		    	if(data.message == "-1") {
+		    		//error handling
+		    		$status.find("p").text("Invalid url.");
+		    		return;
+		    	}
+		    	var videos = data.message;
+		    	var type;
+		    	for(var i = 0; i < videos.length; i++) {
+		    		type = videos[i].container;
+		    		if(type == "webm" || type == "mp4") {
+		    			dataUri = "data:" + container[type] + ";base64,";
+		    			source = videos[i].url;
+		    			downloadAudio();
+		    			//console.log(source);
+		    			return;
+		    		}
+		    	}
+
+		    	//if a valid video type could not be found:
+		    	$status.find("A valid video type could not be found");
+		    });
+		});
+	}	
+	function downloadAudio() {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', source, true);
+		xhr.responseType = 'arraybuffer';
+		xhr.onload = function(e) {
+			if(this.status == 200) {
+				dataUri = dataUri + base64ArrayBuffer(xhr.response);
+				loadAudio();
+			}
+		}
+		xhr.send();
+	}
 }
+
+
+
+
