fe8754753ed768341c79c03779240f630ff8f2dc
Song Chen <schen2315@bths.edu>
2017-04-13 16:02:53 -0400

    porting code over to background page

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-// var sound = new Howl({
-//   src: ['https://r11---sn-ab5l6n7d.googlevideo.com/videoplayback?itag=22&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&lmt=1472373274637338&mt=1490066782&ip=149.125.66.95&key=yt6&mime=video%2Fmp4&expire=1490088486&id=o-AJpXGHKA-AswxRpFysqXA4__WZqqluB_4gbTMx9TsXMK&upn=Rlw9a2yphuA&beids=%5B9466591%5D&mn=sn-ab5l6n7d&mm=31&ms=au&mv=m&ratebypass=yes&source=youtube&pl=22&initcwndbps=1208750&dur=225.697&requiressl=yes&ipbits=0&signature=4A077F7CAE1F5E0D26C917BECEAFE8516CF80F32.4E1402C60ED443BD5A95612A21ABE7D15FADDC1B']
-// });
-// var sound = new Howl({
-// 	src: ['afraidofheights.mp4']
-// })
-// sound.on('play', function() {
-// 	$("p").text("now playing");
-// })
-// $(document).ready(function() {
-// 	sound.play();
-// });
 var baseurl = "https://ixwyi5tpad.execute-api.us-east-1.amazonaws.com/dev";
 var mediaTypes = ["audio/webm", "audio/ogg", "audio/x-ogg", "audio/mp4", "audio/m4a", "audio/mp3", "audio/mpeg", "audio/wav", "audio/x-wav", "audio/flac", "audio/x-flac"];
 var container = {
 	"webm": "audio/webm",
 	"mp4": "audio/mp4"
 }
-// var url = "https://r11---sn-ab5l6n7d.googlevideo.com/videoplayback?source=youtube&mn=sn-ab5l6n7d&dur=225.697&ip=149.125.64.168&initcwndbps=1477500&ipbits=0&pl=22&mv=m&ms=au&mm=31&key=yt6&id=o-AHnpAFumeV_G2kfNE17c3jHWa1XM7wmpGIGFdXGe9EVW&sparams=dur%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&upn=EnxqBf86eeU&mime=video%2Fmp4&itag=22&requiressl=yes&lmt=1472373274637338&expire=1490689859&mt=1490668164&ratebypass=yes&signature=112A7B53E77D270D8857267C1A9B00D5082E2035.2CE6014BBD46E8D7E742643C08805CED6FCA7521";
-var url = "afraidofheights.mp4";
 var sound;
 var duration;
 var base64;
@@ -25,25 +11,20 @@ var snippet = undefined;
 /*typing timeout variable*/
 var typeTO;
 var typeTimer = 800;
-// $(document).ready(function() {
-// 	// var query = "https://ixwyi5tpad.execute-api.us-east-1.amazonaws.com/dev?v=T60Du8Py9yA";
-// 	// $.get(query, function(data) {
-// 	// 	console.log(data)
-// 	// })
-// });
+
+var	url = "",	/* the youtube video */
+	source = "", /* where the video itself is stored */
+	dataUri = ""; /* the video */
 $(document).ready(pageLoadComplete);
 function pageLoadComplete() {
+	console.log("page load complete")
 	var $status = $("div.status"),
 	$input = $("div.input"),
 	$start = $($input.find("input")[0]),
 	$stop = $($input.find("input")[1]),
 	$rate_input =  $($input.find("input")[2]),
-	$rate_p = $($input.find("p")[2]),
-	url = "",
-	source = "",
-	dataUri = "";
-	
-
+	$rate_p = $($input.find("p")[2]);
+	$input.hide();
 	init();
 
 	//loadAudio();
@@ -66,7 +47,9 @@ function pageLoadComplete() {
 	// 	console.log("change");
 	// 	//$rate.p.text("rate: " + $rate.input.val());
 	// });
+
 	/*offset and duration should be in milliseconds*/
+	/*load UI*/
 	function loadAudio(offset, duration, rate) {
 			//var dataUri = "data:" + mediaTypes[3] + ";base64," + base64ArrayBuffer(xhr.response);
 			//var dataUri = url;
@@ -156,7 +139,6 @@ function pageLoadComplete() {
 	}
 
 	function init() {
-		$input.hide();
 		chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 		    url = tabs[0].url;
 		    var url_parameters = url.split('?');
@@ -164,12 +146,13 @@ function pageLoadComplete() {
 		    for(var i=1; i < url_parameters.length; i++) {
 		    	url += url_parameters[i];
 		    }
-		    //console.log(url);
+		    //query my aws lambda function for the 
+		    //source url and video properties
 		    $.get(url, function(data) {
 		    	console.log(data);
 		    	if(data.message == "-1") {
 		    		//error handling
-		    		$status.find("p").text("Invalid url.");
+		    		//$status.find("p").text("Invalid url.");
 		    		return;
 		    	}
 		    	var videos = data.message;
@@ -180,13 +163,12 @@ function pageLoadComplete() {
 		    			dataUri = "data:" + container[type] + ";base64,";
 		    			source = videos[i].url;
 		    			downloadAudio();
-		    			//console.log(source);
 		    			return;
 		    		}
 		    	}
 
 		    	//if a valid video type could not be found:
-		    	$status.find("A valid video type could not be found");
+		    	//$status.find("A valid video type could not be found");
 		    });
 		});
 	}	
